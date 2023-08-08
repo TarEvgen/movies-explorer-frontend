86f1e758ea2bd0244ab82297fb7afed3ce4e44f6
Tarasov Evgeniy <mailtarasov@yandex.ru>
2023-08-08 09:41:52 +0700

    add portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,23 @@
 import "./Portfolio.css";
+import ImgLink from "../../images/link-img.svg"
 
 function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
-        <li className="portfolio__name"></li>
+        <li className="portfolio__name">
+          <a className="portfolio__link" href="https://github.com/TarEvgen/how-to-learn">Статичный сайт</a>
+          <img className="portfolio__link-img" src={ImgLink} alt="Изображение стрелки"></img>
+        </li>
+        <li className="portfolio__name">
+          <a className="portfolio__link" href="https://github.com/TarEvgen/russian-travel">Адаптивный сайт</a>
+          <img className="portfolio__link-img" src={ImgLink} alt="Изображение стрелки"></img>
+        </li>
+        <li className="portfolio__name">
+          <a className="portfolio__link" href="https://github.com/TarEvgen/react-mesto-auth">Одностраничное приложение</a>
+          <img className="portfolio__link-img" src={ImgLink} alt="Изображение стрелки"></img>
+        </li>
       </ul>
       
       
