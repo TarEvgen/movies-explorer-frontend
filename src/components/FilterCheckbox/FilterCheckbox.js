26d746b6b1496e172de358a7ab493ccbbc35bdae
Tarasov Evgeniy <mailtarasov@yandex.ru>
2023-08-12 01:13:04 +0700

    add searchform

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -3,15 +3,15 @@ import "./FilterCheckbox.css";
 function FilterCheckbox() {
   return (
     <>
-    <div className="checkbox-block">
-      <label className="checkbox" for="checkbox-short-films">
-        <input
-          className="checkbox__input"
-          id="checkbox-short-films"
-          type="checkbox"
-        ></input>
-        <span className="checkbox__name">Короткометражки</span>
-      </label>
+      <div className="checkbox-block">
+        <label className="checkbox" for="checkbox-short-films">
+          <input
+            className="checkbox__input"
+            id="checkbox-short-films"
+            type="checkbox"
+          ></input>
+          <span className="checkbox__name">Короткометражки</span>
+        </label>
       </div>
     </>
   );
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,25 +1,22 @@
 import "./SearchForm.css";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
-
 function SearchForm() {
   return (
     <section className="search-form">
-    
-      <form className="search-form__search">
-      <div className="search">
-        <input className="search-form__field-search" placeholder="Фильм"
-></input>
+      <form className="search-form__search-bar">
+        <input
+          className="search-form__field-search"
+          placeholder="Фильм"
+        ></input>
         <button className="search-form__button"></button>
-
-        </div>
-        <FilterCheckbox/>
       </form>
-      
-     
-     
+      <div className="search-form__checkbox">
+        <FilterCheckbox />
+      </div>
+      <div className="search-form__line"></div>
     </section>
   );
 }
 
-export default SearchForm
+export default SearchForm;
