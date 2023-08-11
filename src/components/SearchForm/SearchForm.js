b0cf7ce275af79812e3464a8435aff0c4abe14ed
Tarasov Evgeniy <mailtarasov@yandex.ru>
2023-08-11 09:59:22 +0700

    add filtercheckbox

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,17 +1,22 @@
 import "./SearchForm.css";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 
 function SearchForm() {
   return (
     <section className="search-form">
-      
+    
       <form className="search-form__search">
+      <div className="search">
         <input className="search-form__field-search" placeholder="Фильм"
 ></input>
         <button className="search-form__button"></button>
 
-
+        </div>
+        <FilterCheckbox/>
       </form>
+      
+     
      
     </section>
   );
