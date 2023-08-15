86aa45fb067485f9503469f993d65ba340e85ee9
Tarasov Evgeniy <mailtarasov@yandex.ru>
2023-08-15 20:16:08 +0700

    add annimation

--- src/components/HeaderMovies/HeaderMovies.js
+++ src/components/HeaderMovies/HeaderMovies.js
@@ -1,28 +1,47 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 
 import "./HeaderMovies.css";
 
-function HeaderMovies({openMenu}) {
-
-
- 
+function HeaderMovies({ openMenu }) {
+  const location = useLocation();
 
   return (
-    
-          <header className="header header_movies">
-          <Link className="logo" to="/"></Link>
-          
-            <ul className="header__link-list header__link-list_page-movies">
-              
-              <li><Link className="header__link header__link_page-movies" to="/movies">Фильмы</Link></li>
-              <li><Link className="header__link header__link_page-save-movies" to="/saved-movies">Сохранённые фильмы</Link></li>
-            </ul>
-            <Link className="header__link header__link_page-profile" to="/profile">Аккаунт</Link>
-            <button className="header__button-menu" onClick={openMenu}></button>
-          
-          </header>
-        
-    
+    <header className="header header_movies">
+      <Link className="logo" to="/"></Link>
+
+      <ul className="header__link-list header__link-list_page-movies">
+        <li>
+          <Link
+            className={`header-movies__link ${
+              location.pathname === "/movies"
+                ? "header-movies__link_active"
+                : ""
+            }`}
+            to="/movies"
+          >
+            Фильмы
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            className={`header-movies__link ${
+              location.pathname === "/saved-movies"
+                ? "header-movies__link_active"
+                : ""
+            }`}
+            to="/saved-movies"
+          >
+            Сохранённые фильмы
+          </Link>
+        </li>
+      </ul>
+      <Link className="header__link header__link_page-profile" to="/profile">
+        Аккаунт
+      </Link>
+      <button className="header__button-menu" onClick={openMenu}></button>
+    </header>
   );
 }
 
